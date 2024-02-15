1fc84d77e376d5fa84205e09dcc4d22c81a4e007
Fawaztarar <fawaztarar@gmail.com>
2024-02-15 12:26:12 +0000

    Initial Commit

---
+++ Acebook/AcebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcebookApp.swift
+//  Acebook
+//
+//  Created by Fawaz Tarar on 15/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct AcebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
